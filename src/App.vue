e887de7d74396575e5a315a50458c5baf67c9bc6
Daniel Car <53944969+yaytz@users.noreply.github.com>
2021-08-28 11:45:41 -0500

    calculadora
    calculadora

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
-<div class="Home">
-  <img v-for="ima in imm" :key="ima.id" :src="ima" alt="cat">
-</div>
+  
+  
+  
 </template>
-<script>
-import axios from 'axios'
 
-export default{
-  name: 'Home',
-  data(){
-    return{
-      gf: [],
-      imm:[]
-    }
-  },
-  methods:{
-    async getGf(){
-      const response = await axios.get('https://api.giphy.com/v1/gifs/trending?api_key=nEDtSM1UH5SH7YYGPKdYJG2Q32vE547F&limit=10&rating=g')
-      this.gf = response.data
-      for(let i=0; i<10; i++){
-        this.imm[i]=this.gf.data[i].images.downsized.url
-      }
-      console.log(this.gf);
-      //revisar array map, for each, usar un try catch
-    }
-  },
-  created(){
-    this.getGf()
-  }
-}
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
 </script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #ffffff;
+  margin-top: 60px;
+}
+body{
+  background: #ffffff;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,75 @@
+<template>
+
+  <div class="calculadora">
+  <input type="number">
+  
+  <div class="calculadora-body"> 
+  <button class="button">/</button>
+  <button class="button">(</button>
+  <button class="button">)</button>
+  <button class="button">C</button>
+  <button class="button">1</button>
+  <button class="button">2</button>
+  <button class="button">3</button>
+  <button class="button">+</button>
+  <button class="button">4</button>
+  <button class="button">5</button>
+  <button class="button">6</button>
+  <button class="button">-</button>
+  <button class="button">7</button>
+  <button class="button">8</button>
+  <button class="button">9</button>
+  <button class="button">=</button>
+  </div>
+  </div>
+</template>
+
+
+<script setup>
+import { defineProps, reactive } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const state = reactive({ count: 0 })
+</script>
+
+<style scoped>
+a {
+  color: #10b9be;
+}
+
+</style>
+<style scoped>
+.calculadora input{
+  background: #b9d2d3;
+  max-width: 450px;
+  margin: 0 auto;
+  border: 10px solid #111;
+  border-radius: 15px;
+  text-align: left;
+}
+.button{
+  background: rgba(0, 0, 0, 0.5);
+  border: 1px solid #111; 
+  padding: 20px;
+  color: #EEE;
+  border-radius: 10px; 
+  font-size: 22px; 
+  cursor: pointer; 
+}
+.calculadora-body{
+  background: #009196;
+  padding: 20px;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 20px;
+}
+.calculadora{
+  background: #247b7e;
+  max-width: 400px;
+  margin: 0 auto;
+  border: 2px solid #111;
+  border-radius: 5px;}
+</style>
